efb6f6c060f5beca231069848d2843739f4df935
Hagb (Junyu Guo) <hagb@hagb.name>
2025-05-14 12:59:43 +0800

    improve the proof of sPolynomial_ne_zero

--- Groebner/Basic.lean
+++ Groebner/Basic.lean
@@ -330,32 +330,16 @@ theorem degree_eq_zero_iff{f : MvPolynomial σ R} :
     simp [leadingCoeff]
 
 lemma sPolynomial_ne_zero (f g : MvPolynomial σ R) (h : m.sPolynomial f g ≠ 0) :
-    (0 < (m.toSyn <| m.degree f)) ∨  (0 < (m.toSyn <| m.degree g)) := by
+    (0 < (m.toSyn <| m.degree f)) ∨ (0 < (m.toSyn <| m.degree g)) := by
   simp [MonomialOrder.sPolynomial] at h
-  by_contra h_neg
-  push_neg at h_neg
-  rcases h_neg with ⟨h₁, h₂⟩
-  have h2: m.toSyn (m.degree f) ≥  0 := by
-    exact zero_le m (m.toSyn (m.degree f))
-  have h0: m.toSyn (m.degree f) = 0 := by
-    exact le_antisymm h₁ h2
-  have h0': m.degree f = 0 := by
-    exact (AddEquiv.map_eq_zero_iff m.toSyn).mp h0
-  have h3:  m.toSyn (m.degree g) ≥  0 := by
-    exact zero_le m (m.toSyn (m.degree g))
-  have h4: m.toSyn (m.degree g) = 0 := by
-    exact le_antisymm h₂ h3
-  have h4': m.degree g = 0 := by
-    exact (AddEquiv.map_eq_zero_iff m.toSyn).mp h4
-  simp [h0', h4'] at h
-  have h5: f = C (m.leadingCoeff f) := by
-    exact degree_eq_zero_iff.mp h0'
-  have h5': g = C (m.leadingCoeff g) := by
-    exact degree_eq_zero_iff.mp h4'
-  have: C (m.leadingCoeff g) * f - C (m.leadingCoeff f) * g = 0 := by
-    simp [←h5, ←h5']
-    ring
-  ring_nf
+  contrapose! h
+  rcases h with ⟨h₁, h₂⟩
+  rw [← eq_zero_iff, EmbeddingLike.map_eq_zero_iff] at h₁ h₂
+  simp [h₁, h₂]
+  rw [degree_eq_zero_iff] at h₁ h₂
+  nth_rewrite 1 [h₁]
+  nth_rewrite 2 [h₂]
+  ring
 
 /--
 $h_1, h_2 \in k[\mathbf{x}], lm(h_1) = lm(h_2), S(h_1, h_2) \ne 0$, then $lm(S(h_1, h_2)) < lm(h_1)$.
