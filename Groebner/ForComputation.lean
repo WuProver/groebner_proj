f89dee0afc1276d1c44a04eec207bab197587b65
Hagb (Junyu Guo) <hagb@hagb.name>
2025-11-15 16:13:09 +0800

    use `use` before splitting goals

--- Groebner/ForComputation.lean
+++ Groebner/ForComputation.lean
@@ -27,18 +27,18 @@ example :
   -- convert set to `Set.image list.get`
   simp only [← Set.range_get_nil, ← Set.range_get_singleton, ← Set.range_get_cons_list]
   -- use index
-  rw [isRemainder_range_fin]
+  rw [isRemainder_range_fin, ← exists_and_right]
+  use [X 0, X 1 ^ 2, X 2 ^ 3, X 3 ^ 4].get
   split_ands
-  · use [X 0, X 1 ^ 2, X 2 ^ 3, X 3 ^ 4].get
-    split_ands
+  · split_ands
     · simp [Fin.univ_succ, -List.get_eq_getElem, List.get] -- convert sum to add
       try grind-- PIT, we will rely on reflection
-    · intro i
-      fin_cases i
-      all_goals {
-        simp [-List.get_eq_getElem, List.get]
-        try sorry -- compare degree
-      }
+  · intro i
+    fin_cases i
+    all_goals {
+      simp [-List.get_eq_getElem, List.get]
+      try sorry -- compare degree
+    }
   · simp -- here the remainder is 0, whose support set is empty, so `simp` solves it...
 
 set_option linter.unusedSimpArgs false in
@@ -48,16 +48,16 @@ example :
   -- convert set to `Set.image list.get`
   simp only [← Set.range_get_nil, ← Set.range_get_singleton, ← Set.range_get_cons_list]
   -- use index
-  rw [isRemainder_range_fin]
+  rw [isRemainder_range_fin, ← exists_and_right]
+  use [X 3 ^ 4, 0].get
   split_ands
-  · use [X 3 ^ 4, 0].get
-    split_ands
+  · split_ands
     · simp [Fin.univ_succ, -List.get_eq_getElem, List.get] -- convert sum to add
       try grind-- PIT, we will rely on reflection
-    · intro i
-      fin_cases i
-      all_goals {
-        simp [-List.get_eq_getElem, List.get]
-        try sorry -- compare degree
-      }
+  · intro i
+    fin_cases i
+    all_goals {
+      simp [-List.get_eq_getElem, List.get]
+      try sorry -- compare degree
+    }
   · sorry -- we will rely on reflection
